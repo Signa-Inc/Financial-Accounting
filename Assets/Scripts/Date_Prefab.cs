fa0d9082db4bc14d52629b32198e68e400a93819
SignaInc <incsigna009@gmail.com>
2026-02-02 16:41:21 +0300

    Add prefabs with dates

---
+++ Assets/Scripts/Date_Prefab.cs
@@ -0,0 +1,7 @@
+using TMPro;
+using UnityEngine;
+
+public class Date_Prefab : MonoBehaviour
+{
+    public TextMeshProUGUI label;
+}
--- Assets/Scripts/Main_Manager.cs
+++ Assets/Scripts/Main_Manager.cs
@@ -1,3 +1,5 @@
+using NUnit.Framework.Constraints;
+using System;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -20,8 +22,11 @@ public class Main_Manager : MonoBehaviour
     public GameObject error_panel;
     public TextMeshProUGUI error_txt;
 
-    Transaction trans;
-    List<Payment> payments;
+    [HideInInspector, Header("Init Payments")]
+    private List<Payment> payments;
+    private Transaction trans;
+    private List<string> dates = new(); // В 0 индексе самая старая дата
+    private Dictionary<string, List<int>> payment_dic = new();
 
     void Awake() => instance = this;
 
@@ -36,10 +41,7 @@ public class Main_Manager : MonoBehaviour
 
     }
 
-    public void UpdateBalance()
-    {
-        balance_txt.text = $"Balance: {Save_Manager.GetBalance()}";
-    }
+    public void UpdateBalance() => balance_txt.text = $"Balance: {Save_Manager.GetBalance()}";
 
     public void UpdatePayments()
     {
@@ -47,20 +49,52 @@ public class Main_Manager : MonoBehaviour
         for (int i = parent_transform.childCount - 1; i >= 0; i--)
             Destroy(parent_transform.GetChild(i).gameObject);
 
-        SetNewPaymentSizeInContent(true, parent_transform.childCount * 148.3f); // Cчитаем сколько всего объектов и умножаем на их высоту (148.3)
+        SetNewPaymentSizeInContent(true, parent_transform.childCount + dates.Count * 148.3f); // Cчитаем сколько всего объектов и умножаем на их высоту (148.3)
 
-        // Вводим новые платежи
-        payments = Save_Manager.payments;
+        // Узнаем сколько всего у нас есть дат
+        payments = Save_Manager.payments; // Потом будем присваивать транзакции который были совершены за последние 30 дней 
 
-        for (int i = 0; i < payments.Count; i++)
+        foreach(Payment payment in payments)
         {
-            trans = Instantiate(transaction_prefab, parent_transform).GetComponent<Transaction>();
+            if(!dates.Contains(payment.date)){
+                dates.Add(payment.date);
+                SetNewPaymentSizeInContent();
+            }
+        }
 
-            trans.id = payments[i].id;
-            trans.label_txt.text = payments[i].label;
-            trans.price_txt.text = payments[i].price;
+        // Сортируем список по дате (от новой к старой)
+        dates.Sort();
 
-            SetNewPaymentSizeInContent();
+        // Заполняем словарь
+        foreach (string date in dates)
+        {
+            List<int> ids = new List<int>();
+
+            for (int i = 0; i < payments.Count; i++)
+                if(payments[i].date == date)
+                    ids.Add(payments[i].id);
+
+            payment_dic.Add(date, ids);
+
+            //for (int i = 0; i < payment_dic[date].Count; i++)
+            //    print($"Key = {date}, value = {payment_dic[date][i]}");
+        }
+
+        // Создаем префабы дат и транзакций
+        for(int j = 0; j < payment_dic.Count; j++)
+        {
+            Instantiate(date_prefab, parent_transform).GetComponent<Date_Prefab>().label.text = dates[j]; // Устанавливаем дату
+
+            foreach (int item in payment_dic[dates[j]])
+            {
+                trans = Instantiate(transaction_prefab, parent_transform).GetComponent<Transaction>();
+
+                trans.id = payments[item].id;
+                trans.label_txt.text = payments[item].label;
+                trans.price_txt.text = payments[item].price;
+
+                SetNewPaymentSizeInContent();
+            }
         }
     }
 
